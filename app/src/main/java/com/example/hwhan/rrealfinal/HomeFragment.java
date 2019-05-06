fc88bfdd2c070b45b54cf1533af2abab0b74a388
channyoung <45484511+channyoung@users.noreply.github.com>
2019-05-07 01:19:19 +0900

    test
    홈화면 테스트

--- app/src/main/java/com/example/hwhan/rrealfinal/HomeFragment.java
+++ app/src/main/java/com/example/hwhan/rrealfinal/HomeFragment.java
@@ -7,14 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.w3c.dom.DocumentType;
 
 public class HomeFragment extends Fragment {
+
+    TextView textView;
+    Document document;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
-
+        textView= view.findViewById(R.id.showtext);
         return view;
     }
 }
